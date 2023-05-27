715a4d747adc805b6f35d1654cdcea8c24353ca1
Tetiana <t.pasichnyk@ukr.net>
2023-05-28 00:01:22 +0300

    upd: markup

--- src/pages/home/sections/Hero.tsx
+++ src/pages/home/sections/Hero.tsx
@@ -31,19 +31,25 @@ const Hero: FC<Text> = ({ title, subtitle, info }) => {
   return (
     <Row className={"hero"}>
       <Col className={"container"} span={24}>
-        <div className={"img-container"}>
-          <img src={img} alt={"avatar"} />
-        </div>
         <div>
-          <Typography.Title className={"name"} level={1}>
-            {title}
-          </Typography.Title>
-          <Typography.Paragraph className={"subtitle"}>
-            {subtitle}
-          </Typography.Paragraph>
-          <Typography.Paragraph className={'info'}>
-            <span>{info}</span>
-          </Typography.Paragraph>
+          <Row>
+            <Col span={5}>
+              <div className={"img-container"}>
+                <img src={img} alt={"avatar"} />
+              </div>
+            </Col>
+            <Col span={18}>
+              <Typography.Title className={"name"} level={1}>
+                {title}
+              </Typography.Title>
+              <Typography.Paragraph className={"subtitle"}>
+                {subtitle}
+              </Typography.Paragraph>
+              <Typography.Paragraph className={'info'}>
+                <span>{info}</span>
+              </Typography.Paragraph>
+            </Col>
+          </Row>
           <div className={"services"}>
             <Row justify={"space-between"}>
               <Col span={19}></Col>
@@ -53,29 +59,38 @@ const Hero: FC<Text> = ({ title, subtitle, info }) => {
                 </Typography.Paragraph>
               </Col>
             </Row>
+            <Row>
 
-            <div className={"diagram"}>
-              <div>
-                {data.map((it: any, index: any) => {
-                  return (
-                    <Row key={index} justify={"space-between"}>
-                      <Col span={19}>
-                        <Typography.Paragraph>{it.text}</Typography.Paragraph>
-                        <div
-                          style={{ backgroundColor: it.color }}
-                          className={"line"}
-                        ></div>
-                      </Col>
-                      <Col span={5}>
-                        <Typography.Title level={1}>
-                          {it.number}
-                        </Typography.Title>
-                      </Col>
-                    </Row>
-                  );
-                })}
-              </div>
-            </div>
+              <Col span={5}>
+
+              </Col>
+              <Col span={19}>
+                <div className={"diagram"}>
+                  <div>
+                    {data.map((it: any, index: any) => {
+                      return (
+                          <Row key={index} justify={"space-between"}>
+                            <Col className={'diagram-content'} span={19}>
+                              <Typography.Paragraph>{it.text}</Typography.Paragraph>
+                              <div
+                                  style={{ backgroundColor: it.color }}
+                                  className={"line"}
+                              ></div>
+                            </Col>
+                            <Col span={5}>
+                              <Typography.Title level={1}>
+                                {it.number}
+                              </Typography.Title>
+                            </Col>
+                          </Row>
+                      );
+                    })}
+                  </div>
+                </div>
+              </Col>
+            </Row>
+
+
           </div>
           <Row className={"total"}>
             <Col span={19}>
